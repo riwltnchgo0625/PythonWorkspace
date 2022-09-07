9a83ca7b29ba80b591f233473eef1f20f07ea168
riwltnchgo0625 <riwltnchgo0625@naver.com>
2022-09-07 16:44:49 +0900

    .

--- Study20220907/dictionary_ex.py
+++ Study20220907/dictionary_ex.py
@@ -32,11 +32,11 @@ print('------------------------')
 list2 = [1, 2, 3, 4, 5, 6, 7, 8]
 dict4 = {'1월': 31, '2월': 29, '3월': 31, '4월': 30}
 
-a = list2.pop(2)  # 해당 인덱스의 값을 삭제하고 반환 함
-print(list2)
-print(a)
-print(dict4.pop('2월'))
-print(dict4)
+# a = list2.pop(2)  # 해당 인덱스의 값을 삭제하고 반환 함
+# print(list2)
+# print(a)
+# print(dict4.pop('2월'))
+# print(dict4)
 
 print('------------------------')
 
@@ -66,8 +66,46 @@ if '1월' in days_in_month.keys():
 if 28 in days_in_month.values():
     print('28일이 dict에 있습니다.')
 
-days_in_month.clear()  # dict 내용 모두 삭제
-print(days_in_month)
-
-list2.clear()  # 리스트 내용 모두 삭제
+# days_in_month.clear()  # dict 내용 모두 삭제
+# print(days_in_month)
+#
+# list2.clear()  # 리스트 내용 모두 삭제
+# print(list2)
+print('------------------------')
+list2.pop(2)
 print(list2)
+
+print(list2[2])
+
+print('------------------------')
+list3 = [1, 2, 3, 4]
+list4 = [4, 5, 6, 7]
+list5 = list3 + list4  # 리스트의 +연산자는 합집합의 의미는 아니다.
+print(list5)
+
+print('------------------------')
+dict5 = {1: 100, 2: 200, 3: 300}
+dict6 = {1: 1000, 2: 2000, 4: 4000}
+dict5.update(dict6)  # 두개의 dict합칠때는 update사용
+print(dict5)
+
+print('------------------------')
+dict8 = {'1월 월급': 100, '2월 월급': 200, '4월 월급': 300}
+dict9 = {'1월 월급': 150, '3월 월급': 180}
+
+dict8.update(dict9)
+print(dict8)
+
+print('------------------------')
+
+
+def check_and_clear(box):
+    if '불량품' in box.keys():
+        print('불량품이 있으면 box 를 clear합니다.')
+        box.clear()
+
+
+dict10 = {'불량품': 10, '정상품': 50, '수출품': 100}
+check_and_clear(dict10)
+
+print(dict10)
