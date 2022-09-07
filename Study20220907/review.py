61f408c7b4c8d500a03edcabb6949043c1125556
riwltnchgo0625 <riwltnchgo0625@naver.com>
2022-09-07 10:42:46 +0900

    .

---
+++ Study20220907/review.py
@@ -0,0 +1,17 @@
+# def mul(x, y, z):
+#     mul1 = x * y * z
+#     return mul1
+#
+#
+# temp = mul(10, 20, 30)
+# print(temp)
+
+# score = input('점수를 입력하세요: ') #input은 기본적으로 str으로 저장함
+# score = int(score)
+#
+# print(score+100)
+
+list1 = [100, 90, 40, 60, 70]
+
+list2 = list1[3]
+print(list2)
