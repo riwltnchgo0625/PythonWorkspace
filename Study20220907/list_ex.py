d3a7e1060b7b70e2b4342c7cbd9fb7318c3dd4e4
riwltnchgo0625 <riwltnchgo0625@naver.com>
2022-09-07 11:19:10 +0900

    .

--- Study20220907/list_ex.py
+++ Study20220907/list_ex.py
@@ -2,6 +2,9 @@ list1 = [100, 50, 20, 70, 60]
 list2 = [55, 99, 20, 70, 30]
 list5 = [1, 2, 3, 4, 5]
 
+list6 = list1 + list2  # 두개의 리스트 합침
+print(list6)
+
 del list5[2]  # 3번째 지움
 list5.remove(5)  # 지정한 값을 지움
 list5.append(9)  # 리스트 끝에 값 추가
